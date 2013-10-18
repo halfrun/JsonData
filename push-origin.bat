a7346e5a33f1bfab5300e2aea82183bc1e907dc6
= <=>
2013-10-18 15:07:22 -0500

    daily update

--- push-origin.bat
+++ push-origin.bat
@@ -2,6 +2,10 @@
 echo [%date% - %time%] Log Start > C:\programming\pushlog.txt
 echo Current Directory: %CD% > C:\Programming\pushlog.txt
 
+call git config --global user.email = "no@one.com"
+
+call git config --global user.name = "JSON"
+
 call git add . >> c:\programming\pushlog.txt 2>&1
 
 call git commit -m "daily update" >> c:\programming\pushlog.txt 2>&1
