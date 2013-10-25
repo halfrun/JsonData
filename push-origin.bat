b8e119cf6cca4a05dc3f5ac3f2a2a11719d49ead
pudds <pudds55@gmail.com>
2013-10-25 08:17:38 -0500

    daily update

--- push-origin.bat
+++ push-origin.bat
@@ -3,7 +3,5 @@ echo [%date% - %time%] Log Start > C:\SportsDataBots\pushlog.txt
 echo Current Directory: %CD% > C:\SportsDataBots\pushlog.txt
 
 call git add . >> C:\SportsDataBots\pushlog.txt 2>&1
-
 call git commit -m "daily update" >> C:\SportsDataBots\pushlog.txt 2>&1
-
 call git push origin master >> C:\SportsDataBots\pushlog.txt 2>&1
